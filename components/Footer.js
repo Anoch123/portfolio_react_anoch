a14ad5045e6f8e89ce49cabdd679af1155fa5952
Anoch Dissanayake <anochdissanayake@gmail.com>
2023-07-04 08:28:02 +0530

    changes made on all sections of the website

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className='flex flex-col text-black-100  mt-5 border-t border-gray-100'>
       <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
-        <p>@{new Date().getFullYear()} Anoch Dissanayake. All rights reserved</p>
+        <p className='text-center'>@{new Date().getFullYear()} Anoch Dissanayake. All rights reserved</p>
 
         <div className="footer__copyrights-link">
           <Link href="https://github.com/Anoch123" className="text-gray-500">
