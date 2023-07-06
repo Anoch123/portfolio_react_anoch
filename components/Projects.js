c9f64e3128ac4855d2e1ec6179dadff6797e3798
Anoch Dissanayake <anochdissanayake@gmail.com>
2023-07-06 21:52:59 +0530

    changes made on all sections of the website

--- components/Projects.js
+++ components/Projects.js
@@ -18,11 +18,6 @@ const Projects = () => {
                     <div
                       className="absolute top-0 right-0 bottom-0 left-0 h-full w-full bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100 bg-[hsla(0,0%,98.4%,0.2)]"></div>
                   </a>
-                  <svg className="absolute left-0 bottom-0  light:text-neutral-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                    <path fill="currentColor"
-                      d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
-                    </path>
-                  </svg>
                 </div>
                 <div className="p-6">
                   <h5 className="mb-4 text-lg font-bold">{p.title}</h5>
