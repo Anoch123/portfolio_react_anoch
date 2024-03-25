282bf0c9aef9969074a4455f0866427c6f13cc49
Anoch Dissanayake <anochdissanayake@gmail.com>
2024-03-25 22:57:44 +0530

    Added ne projects which are on progress

--- components/constants/constants.js
+++ components/constants/constants.js
@@ -2,9 +2,23 @@ export const projects = [
     {
       title: 'Next JS Portfolio Website',
       description: "I've developed my portfolio which you are looking with the help of Next js which is the best react js framework in the market right now. I've learned so many stuffs including the state management and many more. Everything which is done in this project is done using the best practises that i've learned so far by developing multiple projects.",
-        image: '/images/portfoliowebsite.png',
-        tags: ['Next JS', 'Tailwind CSS'],
+      image: '/images/portfoliowebsite.png',
+      tags: ['Next JS', 'Tailwind CSS', 'Javascript'],
       id: 0,
+    },
+    {
+      title: 'Next JS E-Commerce Website Client Side',
+      description: "Worked on an e-commerce website developed using Next JS Framework and MYSQL as the database. I've used typescript to work on this project so that i could get a basic idea of how typescript work, and what are the differences in Javascript and Typescript.",
+      image: '/images/ecommerce_client_side.png',
+      tags: ['Next JS', 'Tailwind CSS', 'Typescript', 'MYSQL'],
+      id: 1,
+    },
+    {
+      title: 'Next JS E-Commerce Admin Panel for the Client view',
+      description: "I've developed an admin panel for the e-commerce project i've been working on. this admin panel though me how to seamlessly communicate with the e-commerce client side with just an api created using the this admin panel. i've had the ability to learn react hook form, zod, cloudinary and an ORM named prisma",
+      image: '/images/ecommerce_admin_panel.png',
+      tags: ['Next JS', 'Typescript', 'MYSQL', 'Prisma', 'Cloudinary'],
+      id: 2,
     },
   ];
 
