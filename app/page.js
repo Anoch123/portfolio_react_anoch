5f9ea1deff847c998aa1424e34c66a6900fed599
SLM-Anoch <anoch@slmobility.com>
2024-10-22 20:13:11 +0530

    Intergrate Superbase PostgreSQL to the Project and Fetched data to Hero Section

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
-import About from "@/components/About";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
-import Hero from "@/components/Hero";
-import Projects from "@/components/Projects";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Projects from "../components/Projects";
 
 export default function Home() {
   return (
