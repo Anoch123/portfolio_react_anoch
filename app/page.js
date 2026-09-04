6af51d96b380e10685e2befd88755752dd9349a6
anoch123 <anochdissanyake@gmail.com>
2026-09-04 21:12:36 +0530

    fix sharp for netlify

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Experience from "../components/Experience";
-
+// todo
 export default function Home() {
   return (
     <main className="overflow-hidden">
